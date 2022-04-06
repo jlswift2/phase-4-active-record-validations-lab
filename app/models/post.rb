33194390affff5180a52443da5eb2d4ae914edaf
Justin Swift <jlswift2@gmail.com>
2022-04-06 09:36:10 -0500

    commit tree 6b88305e1fe0275be425b5af445bf7908057bbc7

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        if [/Won't Believe | Secret | Guess | Top / ].none? { |exp| exp.match(title) }
+            errors.add(:title, "Clickbait titles only, please")
+        end
+    end
+
 end
